b123e146bee54ac75e25260047b535c45b6d7906
DAFFA AQILAH <daffaqillahhh@365.telkomuniversity.ac.id>
2022-04-02 12:11:44 +0700

    Penambahan class GlosarryItem

---
+++ GlossaryItem1302200097.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul6_Kelompok_08
+{
+    internal class GlossaryItem1302200097
+    {
+
+        String jsonString = File.ReadAllText("C://users//NITRO////source//repos//modul6_Kelompok_08//jurnal6_3_1302200097.json");
+
+        dynamic data3 = JsonConvert.DeserializeObject(jsonString);
+
+        Console.WriteLine("Glossary : " + data3.title);
+		Console.WriteLine("Glossary Entry : " + data3.ID);
+    }
+}
