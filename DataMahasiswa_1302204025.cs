572505b1e24b37370cc11a03bac53f0e250d4868
ErifkaFaodzi <erifka@student.telkomuniversity.ac.id>
2022-04-02 11:57:49 +0700

    Modul6_Kelompok08

---
+++ DataMahasiswa_1302204025.cs
@@ -0,0 +1,33 @@
+﻿using DocumentFormat.OpenXml.Office.CustomUI;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul6_Kelompok_08
+{
+    internal class DataMahasiswa_1302204025
+    {
+        public static void Main()
+        {
+			string jsonstring = File.ReadAllText("C://Users//Faodzi.23//Source//Repos//MOD6_KELOMPOK08//modul6_Kelompok_08//jurnal6_1_1302204025");
+
+			dynamic data = JsonConvert.DeserializeObject(jsonstring);
+
+			Console.WriteLine("Nama : " + data.firstName + " " + data.lastName);
+			Console.WriteLine("Gender : " + data.gender);
+			Console.WriteLine("Umur : " + data.age);
+			Console.WriteLine("Alamat : " + data.address.streetAddress + ", " + data.address.city + ", " + data.address.state);
+			Console.WriteLine("Mata Kuliah : ");
+			foreach (var item in data.courses)
+			{
+				Console.WriteLine();
+				Console.WriteLine("Kode matkul : " + item.code);
+				Console.WriteLine("Nama matkul : " + item.name);
+			}
+			Console.WriteLine();
+		}
+	}
+}
